d4502dc39eb60536b1593c5c2270cbe73f20b8ad
KauanFix <kauan_fix@estudante.sesisenai.org.br>
2026-08-12 09:30:58 -0300

    add - Prepared Statement adicionado ao editar

--- public/editar.php
+++ public/editar.php
@@ -3,10 +3,16 @@
 include "../infra/conexao.php";
 
 $id = $_GET["id"];
-$sql = "SELECT * FROM livros WHERE id = $id";
-$resultado = mysqli_query($conexao, $sql );
 
-$livro =mysqli_fetch_assoc($resultado);
+$stmt = $conexao->prepare(
+    "SELECT * FROM livros WHERE id = ?"
+);
+
+$stmt->bind_param("i", $id);
+$stmt->execute();
+
+$resultado = $stmt->get_result();
+$livro = $resultado->fetch_assoc();
 
 ?>
 
